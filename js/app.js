c667f7d95a1d7a8123f11dd08fd16515731df250
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-29 14:17:51 -0700

    added the remaining quiz questions and alerts, as well a welcome alert based on the user's name given when prompted

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var userName = prompt('Hey! What\'s your name?')
+    alert('Welcome to my About Me page, ' + userName);
+    console.log('This user\'s name is: ' + userName);
+
 var myPets = prompt('Do I have pets?')
 
 // if(myPets.toLowerCase() === 'yes' || myPets.toLowerCase() === 'y'){
@@ -25,6 +29,38 @@ if(myPets.toLowerCase() === 'yes' || myPets.toLowerCase() === 'y'){
 var myGradYear = prompt('Was my highschool graduation year 2016?')
 
 if(myGradYear.toLowerCase() === 'no' || myGradYear.toLowerCase() === 'n'){
+    alert('you are correct!');
+} else {
+    alert('ooops wrong!');
+}
+
+// var myHomeTown = prompt('Did I grow up on Long Island?')
+
+// if(myHomeTown.toLowerCase() === 'yes' || myHomeTown.toLowerCase() === 'y'){
+//     console.log('you are correct!');
+// } else {
+//     console.log('ooops wrong!');
+// }
+
+var myHomeTown = prompt('Did I grow up on Long Island?')
+
+if(myHomeTown.toLowerCase() === 'yes' || myHomeTown.toLowerCase() === 'y'){
+    alert('you are correct!');
+} else {
+    alert('ooops wrong!');
+}
+
+// var jobBackground = prompt('Did I used to work in accounting?')
+
+// if(jobBackground.toLowerCase() === 'no' || jobBackground.toLowerCase() === 'n'){
+//     console.log('you are correct!');
+// } else {
+//     console.log('ooops wrong!');
+// }
+
+var jobBackground = prompt('Did I used to work in accounting?')
+
+if(jobBackground.toLowerCase() === 'no' || jobBackground.toLowerCase() === 'n'){
     alert('you are correct!');
 } else {
     alert('ooops wrong!');

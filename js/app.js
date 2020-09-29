b29fc1b42c5802f6b409b19869a98d391f5b490c
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-29 14:05:53 -0700

    made html structer and added first two quiz question prompts, alerts, and console logs to js file, as well as linked the js and css pages to the index

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var myPets = prompt('Do I have pets?')
+
+// if(myPets.toLowerCase() === 'yes' || myPets.toLowerCase() === 'y'){
+//     console.log('you are correct!');
+// } else {
+//     console.log('ooops wrong!');
+// }
+
+if(myPets.toLowerCase() === 'yes' || myPets.toLowerCase() === 'y'){
+    alert('you are correct!');
+} else {
+    alert('ooops wrong!');
+}
+
+// var myGradYear = prompt('Was my highschool graduation year 2016?')
+
+// if(myGradYear.toLowerCase() === 'no' || myGradYear.toLowerCase() === 'n'){
+//     console.log('you are correct!');
+// } else {
+//     console.log('ooops wrong!');
+// }
+
+var myGradYear = prompt('Was my highschool graduation year 2016?')
+
+if(myGradYear.toLowerCase() === 'no' || myGradYear.toLowerCase() === 'n'){
+    alert('you are correct!');
+} else {
+    alert('ooops wrong!');
+}

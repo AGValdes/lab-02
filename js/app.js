ac383d4e9db65cd0d4fb644ebbb98e324540d156
Ameilia Valdes <valdes.ameilia@gmail.com>
2020-09-30 15:44:33 -0700

    added a guessing game, to much exhaustion, where there can be multiple correct answers, stored in an array using a for loop nested in a while loop. also added some css styling

--- js/app.js
+++ js/app.js
@@ -1,67 +1,99 @@
 'use strict';
-
+var correctAnswerCount = 0;
 var userName = prompt('Hey! What\'s your name?')
     alert('Welcome to my About Me page, ' + userName);
     console.log('This user\'s name is: ' + userName);
 
 var myPets = prompt('Do I have pets?')
 
-// if(myPets.toLowerCase() === 'yes' || myPets.toLowerCase() === 'y'){
-//     console.log('you are correct!');
-// } else {
-//     console.log('ooops wrong!');
-// }
 
 if(myPets.toLowerCase() === 'yes' || myPets.toLowerCase() === 'y'){
     alert('you are correct!');
+    correctAnswerCount++;
+    //     console.log('you are correct!');
 } else {
     alert('ooops wrong!');
+    //     console.log('ooops wrong!');
 }
 
-// var myGradYear = prompt('Was my highschool graduation year 2016?')
-
-// if(myGradYear.toLowerCase() === 'no' || myGradYear.toLowerCase() === 'n'){
-//     console.log('you are correct!');
-// } else {
-//     console.log('ooops wrong!');
-// }
 
 var myGradYear = prompt('Was my highschool graduation year 2016?')
 
 if(myGradYear.toLowerCase() === 'no' || myGradYear.toLowerCase() === 'n'){
     alert('you are correct!');
+    correctAnswerCount++;
+    //     console.log('you are correct!');
 } else {
     alert('ooops wrong!');
+    //     console.log('ooops wrong!');
 }
 
-// var myHomeTown = prompt('Did I grow up on Long Island?')
-
-// if(myHomeTown.toLowerCase() === 'yes' || myHomeTown.toLowerCase() === 'y'){
-//     console.log('you are correct!');
-// } else {
-//     console.log('ooops wrong!');
-// }
 
 var myHomeTown = prompt('Did I grow up on Long Island?')
 
 if(myHomeTown.toLowerCase() === 'yes' || myHomeTown.toLowerCase() === 'y'){
     alert('you are correct!');
+    correctAnswerCount++;
+    //     console.log('you are correct!');
 } else {
     alert('ooops wrong!');
+    //     console.log('ooops wrong!');
 }
 
-// var jobBackground = prompt('Did I used to work in accounting?')
-
-// if(jobBackground.toLowerCase() === 'no' || jobBackground.toLowerCase() === 'n'){
-//     console.log('you are correct!');
-// } else {
-//     console.log('ooops wrong!');
-// }
 
 var jobBackground = prompt('Did I used to work in accounting?')
 
 if(jobBackground.toLowerCase() === 'no' || jobBackground.toLowerCase() === 'n'){
     alert('you are correct!');
+    correctAnswerCount++;
+    //     console.log('you are correct!');
 } else {
     alert('ooops wrong!');
+        //     console.log('ooops wrong!');
 }
+
+var correctNumber = 7;
+for (var i = 0; i < 4; i++){
+    var userGuess = prompt ('Guess a number between 1 and 10');
+    if((i === 3) && (correctNumber !== parseInt(userGuess))) {
+        alert('sorry! you are out of chances');
+        break;
+    }
+    else if (parseInt(userGuess) === correctNumber) {
+        alert('Ay! You got it!');
+        break;
+        correctAnswerCount++;
+    }
+    else if (userGuess !== correctNumber) {
+        alert('Nope, try again!')
+    }
+}
+// seventh question
+var tenFavoriteGames = new Array('eso', 'mariokart', 'animal crossing', 'outer worlds', 'pokemon: yellow', 'super mario sunshine', 'pokemon stadium', 'skyrim', 'mario odyssy', 'pokemon snap');
+
+var guesses = 0;
+var maxGuesses = 6;
+var isAnswerCorrect = false;
+
+while (!isAnswerCorrect && guesses < maxGuesses){
+   
+    var userAnswer = prompt('Name one of my favorite video games!');
+
+    for (var i = 0; i < tenFavoriteGames.length; i++) {
+        if (userAnswer.toLowerCase() === tenFavoriteGames[i]){
+            alert('You got one!');
+            isAnswerCorrect = true;
+            correctAnswerCount++;
+            break;
+        } else if (i === tenFavoriteGames.length - 1) {
+            alert('That one is cool too, just not in the top 10!');
+        }
+    } 
+    guesses++;
+}
+
+if (!isAnswerCorrect) {
+    alert('You\'re out of guesses, sorry!');
+}
+
+alert('You have gotten ' + correctAnswerCount + ' answer(s) correct out of 7!');
